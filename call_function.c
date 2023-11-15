ad333be4415364ba9a5f5e4cbd74a2f61c3662a7
tech7895 <techhunter990@gmail.com>
2023-11-15 05:23:40 -0800

    Task update

--- call_function.c
+++ call_function.c
@@ -11,31 +11,31 @@
  */
 void function_call(op_func func, char *op, char *val, int ln, int format)
 {
-        stack_t *node;
-        int flag;
-        int i;
+	stack_t *node;
+	int flag;
+	int i;
 
 	flag = 1;
-        if (strcmp(op, "push") == 0)
-        {
-                if (val != NULL && val[0] == '-')
-                {
-                        val = val + 1;
-                        flag = -1;
-                }
-                if (val == NULL)
-                        err(5, ln);
-                for (i = 0; val[i] != '\0'; i++)
-                {
-                        if (isdigit(val[i]) == 0)
-                                err(5, ln);
-                }
-                node = create_node(atoi(val) * flag);
-                if (format == 0)
-                        func(&node, ln);
-                if (format == 1)
-                        add_to_queue(&node, ln);
-        }
-        else
-                func(&head, ln);
+	if (strcmp(op, "push") == 0)
+	{
+		if (val != NULL && val[0] == '-')
+		{
+			val = val + 1;
+			flag = -1;
+		}
+		if (val == NULL)
+			err(5, ln);
+		for (i = 0; val[i] != '\0'; i++)
+		{
+			if (isdigit(val[i]) == 0)
+				err(5, ln);
+		}
+		node = create_node(atoi(val) * flag);
+		if (format == 0)
+			func(&node, ln);
+		if (format == 1)
+			add_to_queue(&node, ln);
+	}
+	else
+		func(&head, ln);
 }
