8bf01fd1a383e2126aca0767ad5408ded7b20d1e
tech7895 <techhunter990@gmail.com>
2023-11-15 05:36:41 -0800

    header

--- monty.h
+++ monty.h
@@ -2,12 +2,13 @@
 #define MONTY_H
 
 #define _GNU_SOURCE
-#include <stdio.h>
-#include <unistd.h>
+
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <ctype.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -39,43 +40,32 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-extern stack_t *head;
-typedef void (*op_func)(stack_t **, unsigned int);
 
-/*file operations*/
 void _fileop(char *file_name);
 int _parse(char *buffer, int line_number, int format);
 void _fileread(FILE *);
 int len_chars(FILE *);
 void find_func(char *, char *, int, int);
-
-/*Stack operations*/
 stack_t *create_node(int n);
 void _nodfree(void);
 void _stackprnt(stack_t **, unsigned int);
+extern stack_t *head;
+typedef void (*op_func)(stack_t **, unsigned int);
 void _stackadd(stack_t **, unsigned int);
 void add_to_queue(stack_t **, unsigned int);
-
 void function_call(op_func, char *, char *, int, int);
-
 void print_top(stack_t **, unsigned int);
 void pop_top(stack_t **, unsigned int);
 void nop(stack_t **, unsigned int);
 void swap_nodes(stack_t **, unsigned int);
-
-/*Math operations with nodes*/
 void b_add(stack_t **, unsigned int);
 void b_sub(stack_t **, unsigned int);
 void b_div(stack_t **, unsigned int);
 void b_mul(stack_t **, unsigned int);
 void b_mod(stack_t **, unsigned int);
-
-/*String operations*/
 void ch_print(stack_t **, unsigned int);
 void str_print(stack_t **, unsigned int);
 void rotl(stack_t **, unsigned int);
-
-/*Error hanlding*/
 void err(int error_code, ...);
 void err_plus(int error_code, ...);
 void string_err(int error_code, ...);
