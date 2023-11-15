30af95f3015fd5e5eb8b795f21bfa076a660dd0c
tech7895 <techhunter990@gmail.com>
2023-11-15 05:55:25 -0800

    Error hndler

--- handle_err.c
+++ handle_err.c
@@ -1,27 +1,27 @@
 #include "monty.h"
 
 /**
- * err - This function prints appropiate error messages
+ * err - This function prints appropiate error messges
  * determined by their error code
- * @error_code: The error codes include:
- * (1) => The user does not give any file or more than one file to the program.
- * (2) => The file provided is not a file that can be opened or read.
- * (3) => The file provided contains an invalid instruction.
- * (4) => When the program is unable to malloc more memory.
- * (5) => When the parameter passed to the instruction "push" is not an int.
- * (6) => When the stack it empty for pint.
- * (7) => When the stack it empty for pop.
- * (8) => When stack is too short for operation
+ * @error_code: The error codes:
+ * (1) => no input file or only one input file
+ * (2) => The file can't be opened or read
+ * (3) => The file contains invalid instruction
+ * (4) => the program is unable to allocate memory
+ * (5) => the parameter passed is not an int
+ * (6) => the stack is empty for pint
+ * (7) => the stack it empty for pop
+ * (8) => the stack is too short
  *
  * Return: void
  */
 void err(int error_code, ...)
 {
-	va_list ag;
+	va_list g;
 	char *op;
-	int l_num;
+	int number_l;
 
-	va_start(ag, error_code);
+	va_start(g, error_code);
 	switch (error_code)
 	{
 		case 1:
@@ -29,18 +29,18 @@ void err(int error_code, ...)
 			break;
 		case 2:
 			fprintf(stderr, "Error: Can't open file %s\n",
-				va_arg(ag, char *));
+				va_arg(g, char *));
 			break;
 		case 3:
-			l_num = va_arg(ag, int);
-			op = va_arg(ag, char *);
-			fprintf(stderr, "L%d: unknown instruction %s\n", l_num, op);
+			number_l = va_arg(g, int);
+			op = va_arg(g, char *);
+			fprintf(stderr, "L%d: unknown instruction %s\n", number_l, op);
 			break;
 		case 4:
 			fprintf(stderr, "Error: malloc failed\n");
 			break;
 		case 5:
-			fprintf(stderr, "L%d: usage: push integer\n", va_arg(ag, int));
+			fprintf(stderr, "L%d: usge: push integer\n", va_arg(g, int));
 			break;
 		default:
 			break;
@@ -50,38 +50,39 @@ void err(int error_code, ...)
 }
 
 /**
- * err_plus - handles errors.
- * @error_code: The error codes are the following:
- * (6) => When the stack it empty for pint.
- * (7) => When the stack it empty for pop.
- * (8) => When stack is too short for operation.
- * (9) => Division by zero.
+ * err_plus - This function handles errors
+ * @error_code: The error codes:
+ *
+ * (6) => the stack it empty for pint
+ * (7) => the stack it empty for pop
+ * (8) => the stack is too short for operation
+ * (9) => zero denominator encountered
  */
 void err_plus(int error_code, ...)
 {
-	va_list ag;
+	va_list g;
 	char *op;
-	int l_num;
+	int number_l;
 
-	va_start(ag, error_code);
+	va_start(g, error_code);
 	switch (error_code)
 	{
 		case 6:
 			fprintf(stderr, "L%d: can't pint, stack empty\n",
-				va_arg(ag, int));
+				va_arg(g, int));
 			break;
 		case 7:
 			fprintf(stderr, "L%d: can't pop an empty stack\n",
-				va_arg(ag, int));
+				va_arg(g, int));
 			break;
 		case 8:
-			l_num = va_arg(ag, unsigned int);
-			op = va_arg(ag, char *);
-			fprintf(stderr, "L%d: can't %s, stack too short\n", l_num, op);
+			number_l = va_arg(g, unsigned int);
+			op = va_arg(g, char *);
+			fprintf(stderr, "L%d: can't %s, stack too short\n", number_l, op);
 			break;
 		case 9:
 			fprintf(stderr, "L%d: division by zero\n",
-				va_arg(ag, unsigned int));
+				va_arg(g, unsigned int));
 			break;
 		default:
 			break;
@@ -91,25 +92,26 @@ void err_plus(int error_code, ...)
 }
 
 /**
- * string_err - handles errors.
- * @error_code: The error codes are the following:
- * (10) ~> The number inside a node is outside ASCII bounds.
- * (11) ~> The stack is empty.
+ * string_err - This function handles errors
+ * @error_code: The error codes:
+ * (10) ~> The number is outside ASCII bounds
+ * (11) ~> empty stack
+ *
  */
 void string_err(int error_code, ...)
 {
-	va_list ag;
-	int l_num;
+	va_list g;
+	int number_l;
 
-	va_start(ag, error_code);
-	l_num = va_arg(ag, int);
+	va_start(g, error_code);
+	number_l = va_arg(g, int);
 	switch (error_code)
 	{
 		case 10:
-			fprintf(stderr, "L%d: can't pchar, value out of range\n", l_num);
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", number_l);
 			break;
 		case 11:
-			fprintf(stderr, "L%d: can't pchar, stack empty\n", l_num);
+			fprintf(stderr, "L%d: can't pchar, stack empty\n", number_l);
 			break;
 		default:
 			break;
